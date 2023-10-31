fef28053067578ae685ee0ef492b6d13b730aeb1
Docenko87 <more.67@inbox.ru>
2023-10-31 21:25:42 +0300

    31/10

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", () =>{
-    document.getElementById("burger").addEventListener("click", function() {
-        document.querySelector("header").classList.toggle("open");
-      })
-
-
-})
-
-
+  const menu = document.querySelector('.header__nav');
+  const burger = document.querySelector("#burger");
+  document.querySelector("#burger").addEventListener("click", function() {
     
-    
+      setTimeout(() => {
+          document.querySelector("header").classList.toggle("open");
+      }, 10);
+    })
+
+  document.addEventListener('click', (e) =>{
+      console.log(e)
+      if (e.target.parentNode == burger){
+          return
+      }
+      if (e.target != menu ){
+          document.querySelector("header").classList.remove("open");
+      }
+  })
+
+  })
